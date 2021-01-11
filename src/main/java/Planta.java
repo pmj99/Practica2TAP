75e8b3a82d15aca4b0d9eb0e9d373cc7d2ec39f7
miguel llorens <miguel.llorenscarro@gmail.con>
2021-01-11 17:52:37 +0100

    Implementando Observer Display

--- src/main/java/Planta.java
+++ src/main/java/Planta.java
@@ -1,12 +1,16 @@
 
 
-public class Planta {
+public class Planta implements Observer {
 	
 	
 	
 	private Boton B1;
 	private Boton B2;
 	private Boton B3;
+	
+	private Display D1;
+	private Display D2;
+	private Display D3;
 	
 	public Planta() {
 		
@@ -26,7 +30,26 @@ public class Planta {
 		
 		B3.pulsar(1, plantaLlama, "ascensor3");
 		
-	} 
+	}
+	public void EstadoAscensor1(Ascensor ascensor1,int pisoActual) {
+		
+		D1.MostrarPisoAscensor(ascensor1, pisoActual);
+		
+	}
+	public void EstadoAscensor2(Ascensor ascensor2,int pisoActual) {
+		
+		D2.MostrarPisoAscensor(ascensor2, pisoActual);
+	}
+	public void EstadoAscensor3(Ascensor ascensor3,int pisoActual) {
+		
+		D3.MostrarPisoAscensor(ascensor3, pisoActual);
+		
+	}
+	@Override
+	public void update(int pisoActual) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 	
 	
