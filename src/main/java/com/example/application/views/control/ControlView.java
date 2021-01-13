285882c887b15377d2588ca4a6de19887fd6af6c
Carlos Ruiz <carlos.rzdr@gmail.com>
2021-01-13 21:45:23 +0100

    Vista de control dividida en tabs

--- src/main/java/com/example/application/views/control/ControlView.java
+++ src/main/java/com/example/application/views/control/ControlView.java
@@ -5,11 +5,16 @@ import java.util.Map;
 
 import com.example.application.views.main.MainView;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -24,10 +29,10 @@ public class ControlView extends VerticalLayout{
 		Div pages = new Div();
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		
-		for (int i = 1; i <= 7; i++) {
-			Tab tab = new Tab("Planta " + i);
+		for (int i = 1; i <= 3; i++) {
+			Tab tab = new Tab("Ascensor " + i);
 			Div page = new Div();
-			page.add(plantaLayout());
+			page.add(controlLayout());
 			
 			if(i!=1) page.setVisible(false);
 			
@@ -45,6 +50,37 @@ public class ControlView extends VerticalLayout{
 		this.setHorizontalComponentAlignment(Alignment.CENTER, pages);
 		add(tabs, pages);
 		
+	}
+
+	public Component controlLayout() {
+		
+		TextField pantallaPiso = new TextField();
+		pantallaPiso.setReadOnly(true);
+		pantallaPiso.setLabel("Planta actual");
+		pantallaPiso.setValue("4");
+		pantallaPiso.setWidthFull();		
+		
+		TextField pantallaEmergencia = new TextField();
+		pantallaEmergencia.setLabel("Funcionamiento");
+		pantallaEmergencia.setValue("Correcto");
+		pantallaEmergencia.setReadOnly(true);
+		//pantallaEmergencia.setValue("Emergencia");
+		pantallaEmergencia.setWidthFull();
+		
+		TextField pantallaEstado = new TextField();
+		pantallaEstado.setReadOnly(true);
+		pantallaEstado.setLabel("Estado");
+		pantallaEstado.setValue("En movimiento");
+		//pantallaEstado.setValue("Parado");
+		pantallaEstado.setWidthFull();
+		
+		VerticalLayout controlLayout = new VerticalLayout();
+
+		controlLayout.setHorizontalComponentAlignment(Alignment.CENTER, pantallaPiso);
+		controlLayout.getStyle().set("border", "1px solid #9E9E9E");
+		controlLayout.add(pantallaPiso, pantallaEmergencia, pantallaEstado);
+		
+		return controlLayout;
 	}
 	
 }
