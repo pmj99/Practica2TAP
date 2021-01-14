73cd06205daebefe3ceea1c41c5e85a5c1c1aaf3
Javier Gomez <j4v.gomez@gmail.com>
2021-01-14 09:40:43 +0100

    Cambios estéticos en el código.

--- src/main/java/com/practica2/tap/logic/Building.java
+++ src/main/java/com/practica2/tap/logic/Building.java
@@ -3,31 +3,33 @@ package com.practica2.tap.logic;
 import java.util.ArrayList;
 
 public class Building {
+	protected ArrayList<Floor> floors;
+	protected ArrayList<Elevator> elevators;
 	
-	private ArrayList<Floor> floors;
-	private ArrayList<Elevator> elevator;
-	
-	public Building(ArrayList<Floor> floors, ArrayList<Elevator> elevator) {
+	public Building(ArrayList<Floor> floors, ArrayList<Elevator> elevators) {
 		super();
 		this.floors = floors;
-		this.elevator = elevator;
+		this.elevators = elevators;
 	}
+
 	public Building(int numFloors, int numElevators) {
-		
 		this.floors= new ArrayList<Floor>();
-		this.elevator= new ArrayList<Elevator>();
+		this.elevators = new ArrayList<Elevator>();
 		
-		for(int i=0; i<numFloors;i++) {
-			
+		for (int i = 0; i < numFloors; i++) {
 			this.floors.add(new Floor(i));
-			
 		}
 		
-		for(int i=0; i< numElevators;i++) {
-			
-			this.elevator.add(new Elevator(i));
+		for (int i = 0; i < numElevators; i++) {
+			this.elevators.add(new Elevator(i));
 		}
 	}
 	
-	
+	public ArrayList<Floor> getFloors() {
+		return floors;
+	}
+
+	public ArrayList<Elevator> getElevators() {
+		return elevators;
+	}
 }
--- src/main/java/com/practica2/tap/logic/Floor.java
+++ src/main/java/com/practica2/tap/logic/Floor.java
@@ -1,47 +1,36 @@
 package com.practica2.tap.logic;
 
-public class Floor  {
-	
+public class Floor {
 	private int numFloor;
-	
-	
 	
 	public Floor(int numFloor) {
 		super();
 		this.numFloor = numFloor;
 	}
+
 	public int getNumFloor() {
 		return numFloor;
 	}
+
 	public void setNumFloor(int numFloor) {
 		this.numFloor = numFloor;
 	}
-	public String llamarAscensor(int plantaLlama,Elevator ele) {
-		
-		
-		return ele.select_floor(plantaLlama);
-	
-	}
-	public int  estadoAscensor(Elevator ele) {
-		
-		return ele.getCurrentFloor();
-		
-	}
-	
-	
 
+	public String llamarAscensor(int plantaLlama, Elevator elevator) {
+		return elevator.select_floor(plantaLlama);
+	}
+
+	public int estadoAscensor(Elevator elevator) {
+		return elevator.getCurrentFloor();
+	}
 	
-	public String noise(int plantaLlama, Elevator ele) {
-		// TODO Auto-generated method stub
+	public String noise(int plantaLlama, Elevator elevator) {
+		String message = "OFF";
 		
-		String messege = null;
-		
-		if(plantaLlama == ele.getCurrentFloor()) {
-			
-			messege="ON";
+		if (plantaLlama == elevator.getCurrentFloor()) {
+			message = "ON";
 		}
-		return messege;
-		
-	}
 
+		return message;
+	}
 }
